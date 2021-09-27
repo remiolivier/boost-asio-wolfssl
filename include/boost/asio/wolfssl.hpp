a5280ae29782d877d11f755ee12d2f19949624d1
Remi Olivier <remi_8@hotmail.com>
2021-09-27 11:06:09 -0700

    Initial working version

---
+++ include/boost/asio/wolfssl.hpp
@@ -0,0 +1,15 @@
+#ifndef BOOST_ASIO_WOLFSSL_HPP
+#define BOOST_ASIO_WOLFSSL_HPP
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+# pragma once
+#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
+
+#include <wolfssl/options.h>
+#include <wolfssl/ssl.h>
+#include <wolfssl/openssl/ssl.h>
+#include <wolfssl/openssl/x509v3.h>
+#include <boost/asio/wolfssl/detail/openssl_types.hpp>
+#include <boost/asio/ssl.hpp>
+
+#endif // BOOST_ASIO_WOLFSSL_HPP
